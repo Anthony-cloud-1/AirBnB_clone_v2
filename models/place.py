0e26c9a12026b1e93e0fc347e4f435d9e9e0a399
Anthony Sackey <tonysack13@gmail.com>
2024-03-19 23:49:17 +0000

    air

--- models/place.py
+++ models/place.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This is the place class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from os import getenv
 import models
-
 
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
@@ -48,7 +47,7 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+        rev = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
 
         amenities = relationship("Amenity", secondary=place_amenity,
@@ -57,19 +56,19 @@ class Place(BaseModel, Base):
     else:
         @property
         def reviews(self):
-            """ Returns list of reviews.id """
-            var = models.storage.all()
-            lista = []
-            result = []
-            for key in var:
-                review = key.replace('.', ' ')
-                review = shlex.split(review)
-                if (review[0] == 'Review'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.place_id == self.id):
-                    result.append(elem)
-            return (result)
+            """ Returns the list of reviews.id """
+            vars = models.storage.all()
+            list_a = []
+            res = []
+            for key in vars:
+                rev = key.replace('.', ' ')
+                rev = shlex.split(rev)
+                if (rev[0] == 'Review'):
+                    list_a.append(vars[key])
+            for element in list_a:
+                if (element.place_id == self.id):
+                    res.append(element)
+            return (res)
 
         @property
         def amenities(self):
