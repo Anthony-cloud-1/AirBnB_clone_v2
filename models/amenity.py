aebf46bffd00df4689086c4b83a951ee8808e87f
calvin sewornu <calvinsewornu123@gmail.com>
2024-03-21 00:43:12 +0000

    amenit

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+"""The amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -7,7 +7,7 @@ from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
+    """The class for Amenity
     Attributes:
         name: input name
     """
