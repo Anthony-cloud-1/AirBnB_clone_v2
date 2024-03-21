cbce7aa2dd6bba2661c0526c4900ca8cfcb2a257
calvin sewornu <calvinsewornu123@gmail.com>
2024-03-21 00:52:14 +0000

    city2

--- models/city.py
+++ models/city.py
@@ -18,4 +18,4 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities"
+                          backref="cities")
