1b37c7d6c7ef9a578e661bba03132b039fd12139
calvin sewornu <calvinsewornu123@gmail.com>
2024-03-21 01:09:32 +0000

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
