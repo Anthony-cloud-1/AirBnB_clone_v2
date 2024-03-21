aef14418f7234ab1fa1c85bc05e63262a0761c1c
calvin sewornu <calvinsewornu123@gmail.com>
2024-03-21 00:50:06 +0000

    amenity1

--- models/amenity.py
+++ models/amenity.py
@@ -13,4 +13,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity
+    place_amenities = relationship("Place", secondary=place_amenity)
