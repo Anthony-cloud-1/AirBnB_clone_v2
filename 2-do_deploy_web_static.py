98461136e49a5baa2965c417b09cbc5d20b46827
Anthony Sackey <tonysack13@gmail.com>
2024-04-04 00:48:30 +0000

    air

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["100.25.164.66", "54.175.185.17"]
 
 
 def do_deploy(archive_path):
